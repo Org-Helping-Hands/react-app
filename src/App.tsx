83095f8c7196c3b041dc8fdfb06b8a633ab79c53
thisisommore <ommore501@gmail.com>
2021-03-30 05:44:20 +0000

    Implement routing using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Login } from './components/login/login';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <Login/>
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
