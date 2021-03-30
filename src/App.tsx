80e1b00560640cc1530c6384a8652dfefc19881d
thisisommore <ommore501@gmail.com>
2021-03-30 06:07:01 +0000

    Add route for home

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Login } from './components/login/login';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Home } from './components/home/home';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/login" component={Login} />
+        <Route path="/home" component={Home} />
       </Switch>
     </Router>
   );
