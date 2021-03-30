4c848d4f499f4d5982b57b43cde1d0cea2416a1b
siddheshsurve <sysurve24@gmail.com>
2021-03-30 10:49:35 +0530

    Introduce login page

---
+++ src/components/login/login.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./login.css";
+export class Login extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="circle1">
+          <p>
+            Join <br /> Us
+          </p>
+        </div>
+        <div className="circle2">
+          <p>
+            World
+            <br />
+            Needs
+            <br />
+            Your
+            <br />
+            Help
+          </p>
+        </div>
+        <div className="circle3">
+          <p>
+            Helping
+            <br />
+            Hands
+          </p>
+        </div>
+        <button className="btn-get-started">Get Started</button>
+      </>
+    );
+  }
+}
