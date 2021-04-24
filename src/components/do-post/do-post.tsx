000878a29a90bc4d795774b52902374fa40bbf91
Om More <51229945+thisisommore@users.noreply.github.com>
2021-04-24 11:03:32 +0530

    Introduce DoPost page

---
+++ src/components/do-post/do-post.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./do-post.css";
+export class DoPost extends React.Component {
+  render() {
+    return (
+      <>
+        <p>Hii</p>
+      </>
+    );
+  }
+}
