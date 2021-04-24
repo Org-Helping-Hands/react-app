97088863c8b87e21a2990fc182cd3a155c0b676e
Om More <51229945+thisisommore@users.noreply.github.com>
2021-04-24 11:05:37 +0530

    Home: Fix error

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { getUserData } from "../../common/api";
 import "./home.css";
 export class Home extends React.Component {
-componentDidMount() {
+  componentDidMount() {
     getUserData().then((data) => {
       let {
         name,
@@ -27,59 +27,88 @@ componentDidMount() {
     totalPosts: 0,
     totalPostCompletedByOthers: 0,
   };
-    render() {
-      return (
-        <>
-    <h1>Helping Hands</h1>
-    <div className="container1">
-      <div id="right">
-        <p>
-          <img
-            src="assets/home/request-message.png"
-            alt=""
-            height="100px"
-            width="100px"
-          />
-        </p>
-        <p>Request for needy</p>
-      </div>
-      <div id="left">
-        <p><img src="assets/home/map.png" alt="" height="100px" width="100px" /></p>
-        <p id="t1">Find needy near me</p>
-      </div>
+  render() {
+    return (
+      <>
+        <h1>Helping Hands</h1>
+        <div className="container1">
+          <div id="right">
+            <p>
+              <img
+                src="assets/home/request-message.png"
+                alt=""
+                height="100px"
+                width="100px"
+              />
+            </p>
+            <p>Request for needy</p>
           </div>
+          <div id="left">
+            <p>
+              <img
+                src="assets/home/map.png"
+                alt=""
+                height="100px"
+                width="100px"
+              />
+            </p>
+            <p id="t1">Find needy near me</p>
+          </div>
+        </div>
         <h2>Account</h2>
 
         <div className="container2">
           <div className="line1">
             <div id="name">
-                <p id="cont2text">{this.state.name}</p>
-                <p id="cont2text2">{this.state.totalPostCompletedByOthers}/{this.state.totalPosts}{" "}
-                posts {this.state.totalHelps} helps</p>
-              </div>
-              <div id="nameImg">
-                <p><img src="assets/home/user.png" alt="" height="50px" width="50px" /></p>
-              </div>
-        </div>
-        <div className="line2">
+              <p id="cont2text">{this.state.name}</p>
+              <p id="cont2text2">
+                {this.state.totalPostCompletedByOthers}/{this.state.totalPosts}{" "}
+                posts {this.state.totalHelps} helps
+              </p>
+            </div>
+            <div id="nameImg">
+              <p>
+                <img
+                  src="assets/home/user.png"
+                  alt=""
+                  height="50px"
+                  width="50px"
+                />
+              </p>
+            </div>
+          </div>
+          <div className="line2">
             <div id="email">
-                <p id="cont2text">Email</p>
-                <p id="cont2text2">{this.state.emailId}</p>
-              </div>
-              <div id="emailImg">
-                <p><img src="assets/home/email.png" alt="" height="50px" width="50px" /></p>
-              </div>
-        </div>
-        <div className="line3">
+              <p id="cont2text">Email</p>
+              <p id="cont2text2">{this.state.emailId}</p>
+            </div>
+            <div id="emailImg">
+              <p>
+                <img
+                  src="assets/home/email.png"
+                  alt=""
+                  height="50px"
+                  width="50px"
+                />
+              </p>
+            </div>
+          </div>
+          <div className="line3">
             <div id="noti">
-                <p id="cont2text">Notification</p>
-                <p id="cont2text2">Notifications are on</p>
-              </div>
-              <div id="notiImg">
-                <p>
-                  <img src="assets/home/notification.png" alt="" height="50px" width="50px" />
-                </p>
-              </div>
+              <p id="cont2text">Notification</p>
+              <p id="cont2text2">Notifications are on</p>
+            </div>
+            <div id="notiImg">
+              <p>
+                <img
+                  src="assets/home/notification.png"
+                  alt=""
+                  height="50px"
+                  width="50px"
+                />
+              </p>
+            </div>
+          </div>
         </div>
       </>
     );
