ca20367adc01c6226acd6097823b6e9baf2f6a31
shashank patil <shashankpatil125@gmail.com>
2021-06-09 21:42:51 +0530

    Merge branch 'master' of https://github.com/Org-Helping-Hands/react-app into master

--- src/components/find-needy/find-needy.tsx
+++ src/components/find-needy/find-needy.tsx
@@ -1,66 +1,88 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import styles from "./findNeedy.module.css";
-export class FindNeedy extends React.Component {
-  componentDidMount() {
-    this.initializeMap();
-  }
+import { useSpring, animated } from "@react-spring/web";
+import { useDrag } from "react-use-gesture";
+import { IPostMinimal } from "../../common/interfaces/post";
+import { fetchPost } from "../../common/api";
+import { MapBox } from "../mapbox/mapbox";
+import { getLocation } from "../../common/location";
 
-  async initializeMap() {
-    let { latitude, longitude } = await this.getLocation();
-    mapboxgl.accessToken =
-      "pk.eyJ1Ijoib21tb3JlIiwiYSI6ImNrbnl2M2dwbTFrOXoycHBzN3RzdThvd20ifQ.wZeX_Xe1i3NsILKIEe7M4Q";
-    var map = new mapboxgl.Map({
-      container: "rect",
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [longitude, latitude],
-      zoom: 9,
-    });
-  }
+var map: mapboxgl.Map;
 
-  getLocation() {
-    return new Promise<GeolocationCoordinates>((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          resolve(position.coords);
-        },
-        (err) => reject(err)
-      );
-    });
+export function FindNeedy() {
+  const [posts, setPosts] = useState<IPostMinimal[]>([]);
+  const [markers, setMarkers] = useState<mapboxgl.Marker[]>([]);
+  function createMarkers(posts: IPostMinimal[]) {
+    setMarkers(
+      posts.map((e) => {
+        return new mapboxgl.Marker().setLngLat([
+          parseFloat(e.longitude),
+          parseFloat(e.latitude),
+        ]);
+      })
+    );
   }
-  render() {
-    return (
-      <>
-        <div className={styles.arrow}>
-          <img
-            src="/assets/find-needy/arrow.jpg"
-            alt=""
-            height="31px"
-            width="31px"
-          />
-        </div>
-        <div className={styles.head1}>
+  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 10 }));
+
+  // Set the drag hook and define component movement based on gesture data
+  const bind = useDrag(
+    ({ direction, movement: [mx, my] }) => {
+      if (direction[1] != 0) {
+        api.start({ x: mx, y: my });
+      }
+    },
+    { bounds: { left: 0, right: 0, bottom: 10, top: -500 } }
+  );
+  useEffect(() => {
+    getLocation()
+      .then(({ latitude, longitude }) => {
+        return fetchPost(latitude, longitude);
+      })
+      .then(({ data }) => {
+        setPosts(data);
+        createMarkers(data);
+      });
+  }, []);
+  return (
+    <>
+      <div className={styles.arrow}>
+        <img
+          src="/assets/find-needy/arrow.jpg"
+          alt=""
+          height="31px"
+          width="31px"
+        />
+      </div>
+      <div className={styles.head1}>
         <h1>Needy people near you</h1>
-        </div>
-        
+      </div>
 
-        <div id="rect" className={styles.rect}></div>
+      <MapBox markers={markers}></MapBox>
+      <animated.div style={{ x, y, position: "relative", zIndex: 4 }}>
         <div className={styles.postersList}>
-          <div className={styles.horizontalLine}></div>
-          {[1, 2, 4, 5].map((_, i) => (
-            <div className={styles.line1} key={i}>
+          <div className={styles.horizontalLine} {...bind()}></div>
+          {posts.map((post, i) => (
+            <div className={styles.line1} key={post.id}>
               <div className={styles.name}>
-              <div className={styles.para1}>
-              <p className={styles.cont2text1}>Posted by siddhesh</p>
-              </div>
-              <div className={styles.para2}>
-              <p className={styles.cont2text2}>Food, Water</p>
-              </div>
-               
+                <div className={styles.para1}>
+                  <p className={styles.cont2text1}>
+                    Posted by {post.postedBy.name}
+                  </p>
+                </div>
+                <div className={styles.para2}>
+                  <p className={styles.cont2text2}>
+                    {post.neededItems.map((e, i) => {
+                      let output = "";
+                      if (i > 0) output = ", ";
+                      output += e.itemName;
+                      return output;
+                    })}
+                  </p>
+                </div>
               </div>
               <div className={styles.nameImg1}>
-              
-              <p className={styles.userImg1}>
+                <p className={styles.userImg1}>
                   <img
                     src="/assets/find-needy/user.png"
                     alt=""
@@ -68,13 +90,11 @@ export class FindNeedy extends React.Component {
                     width="46px"
                   />
                 </p>
-              
-                
               </div>
             </div>
           ))}
         </div>
-      </>
-    );
-  }
+      </animated.div>
+    </>
+  );
 }
