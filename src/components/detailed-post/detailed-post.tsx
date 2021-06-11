960051fef7e0ac448ac654a5f7154955a0139a72
241-panu <paritpranav@gmail.com>
2021-06-11 18:45:31 +0530

    edited page

--- src/components/detailed-post/detailed-post.tsx
+++ src/components/detailed-post/detailed-post.tsx
@@ -93,7 +93,7 @@ export function DetailedPost(props: RouteProps) {
           </div>
         </div>
 
-        {/* <div className={styles.mapbox}></div> */}
+       
 
         <MapBox/>   
       
