a027d3b52f4a4a96df3c709ea77745146cf14a15
shashank patil <shashankpatil125@gmail.com>
2021-06-11 18:24:58 +0530

    tag

--- src/components/help/help.tsx
+++ src/components/help/help.tsx
@@ -10,7 +10,14 @@ export class help extends React.Component {
                         src="/assets/find-needy/arrow.jpg" />
                     <p className={styles.top}>Needy people near you</p>
                     <div className={styles.div}></div>
+                    <span>
+                        <i className={`iconify ${styles.tag}`} data-icon="akar-icons:water"></i>
+                        <i className={`iconify ${styles.tag}`} data-icon="emojione-monotone:pot-of-food"></i>
+                        <i className={`iconify ${styles.tag}`} data-icon="mdi-hospital-box"></i>
+                    </span>
                     <p className={styles.bottom}>Needy food and water and shelter will also help. Also it will be nice if you donate your old clothes</p>
+
+
                     <span className={`iconify-wrapper `}>
                         <i className={`iconify ${styles.hicons}`} data-icon="akar-icons:cross"></i>
                     </span>
