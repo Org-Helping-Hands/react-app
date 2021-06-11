785eb6f00127bd8132ae32b72fc779740784f00b
shashank patil <shashankpatil125@gmail.com>
2021-06-11 17:22:32 +0530

    ui design

--- src/components/help/help.tsx
+++ src/components/help/help.tsx
@@ -7,14 +7,17 @@ export class help extends React.Component {
             <>
                 <div>
                     <img className={styles.arrow}
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdRBmWl9vq2wqCJOU6Jql1-3X7fRdCNdBiKKlKxUhAhNlD7My2a9UtGOYxgHrIDzaG8Oc&usqp=CAU" />
+                        src="/assets/find-needy/arrow.jpg" />
                     <p className={styles.top}>Needy people near you</p>
                     <div className={styles.div}></div>
-                    <p className={styles.bottom}>Following location posted by siddhesh</p>
-                    <div className={styles.buttons}>
-                        <button className={styles.btn}>Mark as helped</button>
-                        <button className={styles.btn2}>Mark as missing</button>
-                    </div>
+                    <p className={styles.bottom}>Needy food and water and shelter will also help. Also it will be nice if you donate your old clothes</p>
+                    <span className={`iconify-wrapper `}>
+                        <i className={`iconify ${styles.hicons}`} data-icon="akar-icons:cross"></i>
+                    </span>
+                    <span className={`iconify-wrapper `}>
+                        <i className={`iconify ${styles.shicons}`} data-icon="akar-icons:check" data-inline="false"></i>
+                    </span>
+
                 </div>
 
             </>
